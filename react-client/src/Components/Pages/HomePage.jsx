931dc71c07630c2e3a1aad4a4d0e53b9ff834a9a
Kevin Haro <kharo1@student.gsu.edu>
2018-03-29 22:21:05 -0400

    added semantic UI react

--- react-client/src/Components/Pages/HomePage.jsx
+++ react-client/src/Components/Pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 class HomePage extends Component {
+  
   render() {
     return (
       <div>
-        Home page
+          
       </div>
     )
   }
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import '../semantic/dist/semantic.min.css';
+
 import App from './Components/App';
 ReactDOM.render(<App />, document.getElementById('app'));
