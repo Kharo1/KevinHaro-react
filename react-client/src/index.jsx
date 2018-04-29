bea6dcea91af0e1fa4cc058ef24646550137d47e
Kevin Haro <kharo1@student.gsu.edu>
2018-04-29 16:31:10 -0400

    added css-loader html-webpack-plugin less-loader style-loader url-loader ,all attempting to resolve issue with importing semantic.min.css

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
-import 'semantic-ui-css/semantic.min.css';
+import '../../semantic/dist/semantic.min.css';
+
 ReactDOM.render(<App />, document.getElementById('app'));
